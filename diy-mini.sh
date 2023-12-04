df5a9a8cc86a2584343f7418eff70d5b0385888f
liuyuan29 <58924895+liuyuan29@users.noreply.github.com>
2023-12-04 09:23:45 +0800

    Update diy-mini.sh

--- diy-mini.sh
+++ diy-mini.sh
@@ -20,6 +20,7 @@ rm -rf feeds/luci/applications/luci-app-netdata
 # 添加额外插件
 git clone --depth=1 https://github.com/kongfl888/luci-app-adguardhome package/luci-app-adguardhome
 git clone --depth=1 https://github.com/Jason6111/luci-app-netdata package/luci-app-netdata
+git clone --depth=1 https://github.com/animegasan/luci-app-quickstart package/luci-app-quickstart
 # svn export https://github.com/syb999/openwrt-19.07.1/trunk/package/network/services/msd_lite package/msd_lite
 
 # 科学上网插件
