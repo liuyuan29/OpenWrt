8016d89fb5100287e02d087608601a7a782ad758
liuyuan29 <58924895+liuyuan29@users.noreply.github.com>
2023-12-04 14:26:41 +0800

    Update diy-mini.sh

--- diy-mini.sh
+++ diy-mini.sh
@@ -16,9 +16,10 @@ rm -rf feeds/packages/net/smartdns
 rm -rf feeds/luci/themes/luci-theme-argon
 rm -rf feeds/luci/applications/luci-app-mosdns
 rm -rf feeds/luci/applications/luci-app-netdata
-rm -rf feeds/packages/lean/luci-app-turboacc
-rm -rf feeds/packages/lean/luci-app-unblockmusic
-rm -rf feeds/packages/lean/luci-app-zerotier
+rm -rf feeds/luci/applications/luci-app-unblockmusic
+rm -rf feeds/luci/applications/luci-app-zerotier
+rm -rf feeds/packages/multimedia/UnblockNeteaseMusic
+rm -rf feeds/packages/net/zerotier
 
 # 添加额外插件
 git clone --depth=1 https://github.com/kongfl888/luci-app-adguardhome package/luci-app-adguardhome
